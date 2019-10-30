7c8b63dafb60dc4bf3d4046c9756a491a3824ff2
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-29 21:11:21 -0700

    added a gitignore

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  
-});
+    res.send('hello new node 23');
+})
+//get the persons from the data base
+server.get('/users', (req, res) => {
+
+    db.find()
+    .then(user => {
+        if(user === []) {
+            res.status(404).json({ message: "did not find users"})
+        } else {
+            res.json(user)
+        }
+    })
+})
 
 server.use('/api/posts', postsRouter);
 
